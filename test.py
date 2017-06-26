dce6340806b464f0269feeb6da6a4c9bb28e5a62
liaojiacan <liaojiacan@qq.com>
2017-06-26 14:26:56 +0800

    测试脚本

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-import check
+import login
 import unittest
 
 # 测试类
 class CalTest(unittest.TestCase):
     def test(self):
-        r = check.login("username", "password")
+        r = login.login("username", "password")
         print(r)
 
 if __name__=='__main__':unittest.main()
